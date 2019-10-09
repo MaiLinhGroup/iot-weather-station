1c26eabaad3a4f0a00f6e49e090213286cb4f843
Mai Linh Nguyen <mailinh.nguyen@bcgdv.com>
2019-10-09 23:35:48 +0200

    init iot weather station

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+#include <ESP8266WebServer.h>
+
+#include "main.h"
+
+
+// used GYBMEP BME/BMP280 instead of Adafruit's bme/bmp280
+#define I2C_ADDR (0x76)
+#define CHIP_ID (0x60)
+// August 2019 monthly mean for Germany from dwd.de
+#define SEALVLPRESSURE_HPA (1015.00) /* Adjusted to local forecast! */
+
+Adafruit_BMP280 bmp;
+
+const char* ssid = MY_SSID;
+const char* pwd = MY_PSWD;
+
+float temperature, pressure, altitude;
+
+ESP8266WebServer server(80);
+
+String SendHTML(float temperature,float pressure,float altitude){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>ESP8266 Weather Station</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="<script>\n";
+  ptr +="setInterval(loadDoc,1000);\n";
+  ptr +="function loadDoc() {\n";
+  ptr +="var xhttp = new XMLHttpRequest();\n";
+  ptr +="xhttp.onreadystatechange = function() {\n";
+  ptr +="if (this.readyState == 4 && this.status == 200) {\n";
+  ptr +="document.body.innerHTML =this.responseText}\n";
+  ptr +="};\n";
+  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
+  ptr +="xhttp.send();\n";
+  ptr +="}\n";
+  ptr +="</script>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>ESP8266 Weather Station</h1>\n";
+  ptr +="<p>Temperature: ";
+  ptr +=temperature;
+  ptr +="&deg;C</p>";
+  ptr +="<p>Pressure: ";
+  ptr +=pressure;
+  ptr +="hPa</p>";
+  ptr +="<p>Altitude: ";
+  ptr +=altitude;
+  ptr +="m</p>";
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+void handle_OnConnect() {
+  temperature = bmp.readTemperature();
+  pressure = bmp.readPressure() / 100.0F;
+  altitude = bmp.readAltitude(SEALVLPRESSURE_HPA);
+  server.send(200, "text/html", SendHTML(temperature,pressure,altitude)); 
+
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);  
+
+  Serial.println(F("Weather Station setup."));
+
+  if(bmp.begin(I2C_ADDR,CHIP_ID)) {
+    Serial.println(F("GYBMEP BME/BMP280 found!"));
+  } else {
+    Serial.println(F("No sensor found!"));
+  }
+   
+  
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
+
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, pwd);
+
+  //check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+
+  
---
+++ src/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MY_SSID "your ssid" // << replace with real wifi credential
+#define MY_PSWD "your password" // << replace with real wifi credential
+
+#endif
