20ccef9d044fb8deecc66d1ad874161c09bd459d
Mai Linh Nguyen <mailinh.nguyen@bcgdv.com>
2019-12-28 22:56:46 +0100

    add altitude

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,19 @@
 
 #include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
 
 #include <ESP8266WebServer.h>
 
 #include "main.h"
 
-
-// used GYBMEP BME/BMP280 instead of Adafruit's bme/bmp280
-#define I2C_ADDR (0x76)
-#define CHIP_ID (0x60)
-// August 2019 monthly mean for Germany from dwd.de
-#define SEALVLPRESSURE_HPA (1015.00) /* Adjusted to local forecast! */
-
 void handle_OnConnect();
 void handle_NotFound();
-String SendHTML(float temperature,float pressure,float altitude);
+String SendHTML(float temperature, float humidity, float pressure,float altitude);
 
 
-Adafruit_BMP280 bmp;
-float temperature, pressure, altitude;
+Adafruit_BME280 bme;
+float temperature, humidity, pressure, altitude;
 
 const char* ssid = MY_SSID;
 const char* pwd = MY_PSWD;
@@ -35,20 +28,13 @@ void setup() {
 
   Serial.println(F("Weather Station setup."));
 
-  if(bmp.begin(I2C_ADDR,CHIP_ID)) {
-    Serial.println(F("GYBMEP BME/BMP280 found!"));
-  } else {
-    Serial.println(F("No sensor found!"));
+  while (!bme.begin())
+  {
+    Serial.println(F("No sensor found, please check wiring!"));
+    delay(1500); // wait for 1.5s
   }
-   
+  Serial.println(F("GYBMEP BME/BMP280 found!"));
   
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-
   Serial.println("Connecting to ");
   Serial.println(ssid);
 
@@ -77,18 +63,18 @@ void loop() {
 }
 
 void handle_OnConnect() {
-  temperature = bmp.readTemperature();
-  pressure = bmp.readPressure() / 100.0F;
-  altitude = bmp.readAltitude(SEALVLPRESSURE_HPA);
-  server.send(200, "text/html", SendHTML(temperature,pressure,altitude));
-
+  temperature = bme.readTemperature();
+  humidity = bme.readHumidity();
+  pressure = bme.readPressure() / 100.0F;
+  altitude = bme.readAltitude(SEALVLPRESSURE_HPA);
+  server.send(200, "text/html", SendHTML(temperature, humidity, pressure,altitude));
 }
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-String SendHTML(float temperature,float pressure,float altitude){
+String SendHTML(float temperature, float humidity, float pressure, float altitude) {
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr +="<title>ESP8266 Weather Station</title>\n";
@@ -115,6 +101,9 @@ String SendHTML(float temperature,float pressure,float altitude){
   ptr +="<p>Temperature: ";
   ptr +=temperature;
   ptr +="&deg;C</p>";
+  ptr +="<p>Humidity: ";
+  ptr +=humidity;
+  ptr +="&percnt;</p>";
   ptr +="<p>Pressure: ";
   ptr +=pressure;
   ptr +="hPa</p>";
--- src/main.h
+++ src/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define MY_SSID "your ssid" // << replace with real wifi credential
-#define MY_PSWD "your password" // << replace with real wifi credential
+#define MY_SSID "rpiAP" // << replace with real wifi credential
+#define MY_PSWD "anteater" // << replace with real wifi credential
+
+// August 2019 monthly mean for Germany from dwd.de
+#define SEALVLPRESSURE_HPA (1040.00) /* Adjust this to your local forecast! */
 
 #endif
