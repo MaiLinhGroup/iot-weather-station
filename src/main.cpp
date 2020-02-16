2aff4fddc7efd76bd66c5286957708037148f583
Mai Linh Nguyen <mailinh.nguyen@bcgdv.com>
2020-02-17 00:14:58 +0100

    remove webserver and add mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,27 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BME280.h>
-
-#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #include "main.h"
 
-void handle_OnConnect();
-void handle_NotFound();
-String SendHTML(float temperature, float humidity, float pressure,float altitude);
-
+// declare function signatures
+void read_barometric_sensor();
 
 Adafruit_BME280 bme;
 float temperature, humidity, pressure, altitude;
 
 const char* ssid = MY_SSID;
 const char* pwd = MY_PSWD;
+const char* mqtt_server = MQTT_BROKER_ADDR;
 
-ESP8266WebServer server(80);
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
 
 void setup() {
   // put your setup code here, run once before main loop:
@@ -28,90 +32,49 @@ void setup() {
 
   Serial.println(F("Weather Station setup."));
 
+  // setup sensor
   while (!bme.begin())
   {
     Serial.println(F("No sensor found, please check wiring!"));
     delay(1500); // wait for 1.5s
   }
-  Serial.println(F("GYBMEP BME/BMP280 found!"));
+  Serial.println(F("Sensor GYBMEP BME/BMP280 found!"));
   
+  // setup wifi
   Serial.println("Connecting to ");
   Serial.println(ssid);
 
-  //connect to your local wi-fi network
   WiFi.begin(ssid, pwd);
 
-  //check wi-fi is connected to wi-fi network
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected..!");
+  Serial.println("WiFi connected...!");
   Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
 
-  server.on("/", handle_OnConnect);
-  server.onNotFound(handle_NotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
+  // setup mqtt
+  mqttClient.setServer(mqtt_server, 1883);
 }
 
 void loop() {
   // main code here, run in an endless loop:
-  server.handleClient();
+  mqttClient.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    mqttClient.publish("test", msg);
+  }
 }
 
-void handle_OnConnect() {
+void read_barometric_sensor() {
   temperature = bme.readTemperature();
   humidity = bme.readHumidity();
   pressure = bme.readPressure() / 100.0F;
-  altitude = bme.readAltitude(SEALVLPRESSURE_HPA);
-  server.send(200, "text/html", SendHTML(temperature, humidity, pressure,altitude));
-}
-
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(float temperature, float humidity, float pressure, float altitude) {
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>ESP8266 Weather Station</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="<script>\n";
-  ptr +="setInterval(loadDoc,1000);\n";
-  ptr +="function loadDoc() {\n";
-  ptr +="var xhttp = new XMLHttpRequest();\n";
-  ptr +="xhttp.onreadystatechange = function() {\n";
-  ptr +="if (this.readyState == 4 && this.status == 200) {\n";
-  ptr +="document.body.innerHTML =this.responseText}\n";
-  ptr +="};\n";
-  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
-  ptr +="xhttp.send();\n";
-  ptr +="}\n";
-  ptr +="</script>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<div id=\"webpage\">\n";
-  ptr +="<h1>ESP8266 Weather Station</h1>\n";
-  ptr +="<p>Temperature: ";
-  ptr +=temperature;
-  ptr +="&deg;C</p>";
-  ptr +="<p>Humidity: ";
-  ptr +=humidity;
-  ptr +="&percnt;</p>";
-  ptr +="<p>Pressure: ";
-  ptr +=pressure;
-  ptr +="hPa</p>";
-  ptr +="<p>Altitude: ";
-  ptr +=altitude;
-  ptr +="m</p>";
-  ptr +="</div>\n";
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
 }
--- src/main.h
+++ src/main.h
@@ -3,6 +3,7 @@
 
 #define MY_SSID "rpiAP" // << replace with real wifi credential
 #define MY_PSWD "anteater" // << replace with real wifi credential
+#define MQTT_BROKER_ADDR "broker.mqtt-dashboard.com" // << replace with real broker address
 
 // August 2019 monthly mean for Germany from dwd.de
 #define SEALVLPRESSURE_HPA (1040.00) /* Adjust this to your local forecast! */
